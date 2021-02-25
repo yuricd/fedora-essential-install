a45805f7bded2fa4e058058e724d4bbc6ffa5094
Yuri Delgado <yuricd@bol.com.br>
2021-02-25 18:45:07 -0300

    Remove container after execution

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t fedora_bp_installer:latest .
-docker run -i -t -p 5902:5902 --name fedora_test fedora_bp_installer
+docker run -i -t --rm -p 5902:5902 --name fedora_test fedora_bp_installer
